25cb3e8e496c0b1e122e0fd0fd4753036794b1fa
Riley <rileyjohnson99@hotmail.com>
2020-07-26 21:48:19 -0500

    disabled annoying ESLing rule

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import Layout from "../components/Layout";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello RJ's New Site 👋</h1>
+    <p>If you're seeing this, I'm sorry.</p>
+    <p>
+      I'm working on a CD system for the back-end, so for the time being you'll
+      have to look at this mess until that's squared away.
+    </p>
     <p>
       <Link href="/about">
         <a>About</a>
